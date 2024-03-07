34e0b54c42c65874e63ec955755a41a04e6925e4
Anuradha Ranasinghe <anuranasinghe151@gmail.com>
2024-03-07 16:40:40 +0530

    Feature : add server up and running

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/anuradha151/rss-aggr
 
 go 1.22.0
+
+require (
+	github.com/go-chi/chi v1.5.5 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	godotenv.Load()
+
+	portStr := os.Getenv("PORT")
+	if portStr == "" {
+		log.Fatal("PORT environment variable not set")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portStr,
+	}
+
+	fmt.Println("Server is running on port " + portStr)
+	err := srv.ListenAndServe();
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
